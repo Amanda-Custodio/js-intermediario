e19946b9ef35a4ed710e30c3fffe3246f3278d03
Amanda Custódio <amandaccustodio@hotmail.com>
2022-10-27 12:39:12 -0300

    {Javascript intermediário - aula 01} [27.10.2022 12:40]

---
+++ 01-escopo-javascript/index.js
@@ -0,0 +1,23 @@
+/*
+if(true){
+    const mensagem = "Olá"
+}
+
+console.log(mensagem)
+
+
+function falar(){
+    var mensagem = "Olá, mundo"
+    console.log(mensagem)
+    function falarOutraCoisa(){
+
+    }
+
+    falarOutraCoisa()
+    console.log(falarOutraCoisa)
+}
+
+falar()
+*/
+
+console.log(url)
